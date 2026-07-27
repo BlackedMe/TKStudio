24fc2cc36521428be634d45eebbe70bd4a13793d
Black <smarterss2018@gmail.com>
2026-07-27 13:41:55 +0800

    Feat: Add a file parser

---
+++ include/parser.h
@@ -0,0 +1,6 @@
+#include <string>
+
+namespace TKStudio {
+
+std::string parseFromFile(const std::string& filePath);
+}
---
+++ src/parser.cpp
@@ -0,0 +1,29 @@
+#include "parser.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace TKStudio {
+
+std::string parseFromFile(const std::string& filePath)
+{
+  std::ifstream file;
+
+  file.open(filePath);
+
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file at " << filePath;
+  }
+
+  std::string parsed;
+  std::string buffer;
+
+  while (std::getline(file, buffer)) {
+    parsed += (buffer + '\n');
+  }
+
+  file.close();
+
+  return parsed;
+}
+} // namespace TKStudio
