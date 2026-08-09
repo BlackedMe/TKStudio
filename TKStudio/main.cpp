BlackedMe/TKStudio

commit 1258c1c53c24cccd8a53a2d553c301feb28f2791

Style: Improve the file structure of the project

include/base/application.h
@@ -1,5 +1,8 @@
+#include "TKSpace.h"
 #include <string>
+
 class GLFWwindow;
+class Config;
 
 namespace TKStudio {
 
@@ -16,5 +19,7 @@ public:
 
 private:
   GLFWwindow* m_window;
+
+  TKSpace mySpace;
 };
 } // namespace TKStudio

src/base/application.cpp
@@ -1,4 +1,4 @@
-#include "application.h"
+#include "base/application.h"
 
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
@@ -24,6 +24,11 @@ Application::Application(int width, int height, const std::string& title)
   // Initialize GLAD
   if (!gladLoadGL(glfwGetProcAddress))
     return;
+
+  int x, y;
+  glfwGetFramebufferSize(m_window, &x, &y);
+  glViewport(0, 0, x, y);
+  mySpace.init();
 }
 
 Application::~Application() { glfwTerminate(); }
@@ -35,7 +40,9 @@ void Application::run()
   while (!glfwWindowShouldClose(m_window)) {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glfwSwapBuffers(window);
+    mySpace.drawGrid();
+
+    glfwSwapBuffers(m_window);
 
     glfwPollEvents();
   }

CMakeLists.txt
@@ -40,7 +40,8 @@ glad_add_library(glad_gl_core_46 REPRODUCIBLE API gl:core=4.6)
 #--------------------------------------------------------------------
 
 file(GLOB src "src/*.cpp")
+file(GLOB base "src/base/*.cpp")
 
-add_executable(TKStudio "TKStudio/main.cpp" ${src})
+add_executable(TKStudio "TKStudio/main.cpp" ${src} ${base})
 target_link_libraries(TKStudio glfw glad_gl_core_46)
 target_include_directories(TKStudio PRIVATE "include")

src/base/parser.cpp
@@ -1,4 +1,4 @@
-#include "parser.h"
+#include "base/parser.h"
 #include <fstream>
 #include <iostream>
 #include <string>

commit 49a3802d3f3e625086f758c58a61da96f794866d

Fix: Linking error introduced in 1258c1c5

TKStudio/main.cpp
@@ -1,4 +1,4 @@
-#include "application.h"
+#include "base/application.h"
 
 int main()
 {

include/base/application.h
@@ -1,4 +1,3 @@
-#include "TKSpace.h"
 #include <string>
 
 class GLFWwindow;
@@ -19,7 +18,5 @@ public:
 
 private:
   GLFWwindow* m_window;
-
-  TKSpace mySpace;
 };
 } // namespace TKStudio

src/base/application.cpp
@@ -28,7 +28,6 @@ Application::Application(int width, int height, const std::string& title)
   int x, y;
   glfwGetFramebufferSize(m_window, &x, &y);
   glViewport(0, 0, x, y);
-  mySpace.init();
 }
 
 Application::~Application() { glfwTerminate(); }
@@ -39,8 +38,6 @@ void Application::run()
 
   while (!glfwWindowShouldClose(m_window)) {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    mySpace.drawGrid();
 
     glfwSwapBuffers(m_window);
 